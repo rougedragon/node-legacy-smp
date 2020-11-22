4c149f5349191726cfdeccc68a46f69b2cace009
eperez13 <ethan.perez006@gmail.com>
2020-11-22 13:42:49 +0100

    Cache added

--- src/Advancements.js
+++ src/Advancements.js
@@ -1,25 +1,41 @@
 const fetch = require("node-fetch");
 const cheerio = require('cheerio');
 
+const CACHE_MINUTES = 15;
+var cache = {};
+
 module.exports = class Advancements {
     constructor() {}
 
     async fetchAdvancementsProgression() {
         return new Promise(async (resolve, reject) => {
-            const res = await fetch(
-                "https://legacysmp.com/advancements",
-                {
-                    method: "GET",
+
+            var now = new Date().getTime();
+            if ( ('AdvancementsProgression' in cache) && ( (now - cache['AdvancementsProgression'].timestamp) < (CACHE_MINUTES * 60 * 1000) ) ) {
+                resolve(cache['AdvancementsProgression'].content);
+            }
+            else { 
+                const res = await fetch(
+                    "https://legacysmp.com/advancements",
+                    {
+                        method: "GET",
+                    }
+                )
+                const data = await res.text();
+                let $ = cheerio.load(data);
+                let pourcentage = $('h2');
+                let advancementsCompleted = $('strong')
+                var result = {};
+                result['percentage'] = pourcentage.text();
+                result['completed'] = advancementsCompleted.text().substr(0,advancementsCompleted.text().indexOf(' '));
+
+                cache['AdvancementsProgression'] = {
+                    'timestamp': now,
+                    'content': result
                 }
-            )
-            const data = await res.text();
-            let $ = cheerio.load(data);
-            let pourcentage = $('h2');
-            let advancementsCompleted = $('strong')
-            var result = {};
-            result['percentage'] = pourcentage.text();
-            result['completed'] = advancementsCompleted.text().substr(0,advancementsCompleted.text().indexOf(' '));
-            resolve(result);
+
+                resolve(result);
+            }
         });
     }
 }
