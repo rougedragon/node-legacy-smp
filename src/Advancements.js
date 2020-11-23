e9797c646aa7c98f704541f4755846e55a777efa
eperez13 <ethan.perez006@gmail.com>
2020-11-23 18:06:44 +0100

    latest advancement

--- src/Advancements.js
+++ src/Advancements.js
@@ -13,29 +13,46 @@ module.exports = class Advancements {
             var now = new Date().getTime();
             if ( ('AdvancementsProgression' in cache) && ( (now - cache['AdvancementsProgression'].timestamp) < (CACHE_MINUTES * 60 * 1000) ) ) {
                 resolve(cache['AdvancementsProgression'].content);
+                console.log('From cache');
             }
             else { 
-                const res = await fetch(
-                    "https://legacysmp.com/advancements",
-                    {
-                        method: "GET",
-                    }
-                )
-                const data = await res.text();
-                let $ = cheerio.load(data);
-                let pourcentage = $('h2');
-                let advancementsCompleted = $('strong')
-                var result = {};
-                result['percentage'] = pourcentage.text();
-                result['completed'] = advancementsCompleted.text().substr(0,advancementsCompleted.text().indexOf(' '));
-
-                cache['AdvancementsProgression'] = {
-                    'timestamp': now,
-                    'content': result
-                }
-
-                resolve(result);
+                await this.loadMainPageAdvancements();
+                resolve(cache['AdvancementsProgression'].content);
             }
         });
+    }
+
+    async loadMainPageAdvancements() {
+        var now = new Date().getTime();
+        const res = await fetch(
+            "https://legacysmp.com/advancements",
+            {
+                method: "GET",
+            }
+        )
+        const data = await res.text();
+        let $ = cheerio.load(data);
+        let pourcentage = $('h2');
+        let advancementsCompleted = $('strong');
+        let latestAdvancementDiv = $('.latest');
+        let latestAdvancementTitle = latestAdvancementDiv.children('.card').children('.card-body').children('.card-title').text();
+        let latestAdvancementCondition = latestAdvancementDiv.children('.card').children('.card-body').children('.card-text').html();
+        let latestAdvancementPlayerName = latestAdvancementDiv.children('.card').children('.card-body').children('.card-text').last().children('small').children('a').children('img').attr('alt');
+        let latestAdvancementPlayerImageRef = 'https://legacysmp.com' + latestAdvancementDiv.children('.card').children('.card-body').children('.card-text').last().children('small').children('a').children('img').attr('src');
+        let latestAdvancementTimeCompleted = latestAdvancementDiv.children('.card').children('.card-body').children('.card-text').last().children('small').text();
+        var content = {};
+        content['percentage'] = pourcentage.text();
+        content['completed'] = advancementsCompleted.text().substr(0,advancementsCompleted.text().indexOf(' '));
+        content['latestAdvancementTitle'] = latestAdvancementTitle;
+        content['latestAdvancementCondition'] = latestAdvancementCondition;
+        content['latestAdvancementPlayerName'] = latestAdvancementPlayerName;
+        content['latestAdvancementPlayerImageRef'] = latestAdvancementPlayerImageRef;
+        content['latestAdvancementTimeCompleted'] = latestAdvancementTimeCompleted;
+
+        cache['AdvancementsProgression'] = {
+            'timestamp': now,
+            'content': content
+        }
+        console.log('From request');
     }
 }
